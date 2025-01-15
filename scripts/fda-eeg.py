113f50a9cdcd844afcf97a59a2b8dd958233a85c
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2025-01-14 23:31:07 -0600

    update: added in scripts/fda-eeg.py function to concatenate eeg signals inside subject edf file

--- scripts/fda-eeg.py
+++ scripts/fda-eeg.py
@@ -4,6 +4,7 @@ import re
 import pyedflib
 import numpy as np 
 import matplotlib.pyplot as plt
+import skfda
 
 #NOTE: dataset needs to be downloaded and unziped  
 
@@ -31,8 +32,27 @@ for edf_file_name in eeg_signalfiles_names :
         subject_id = int(re.findall(r"\d\d", file_name)[0])
         after_arith_task[subject_id] = edf_file_name
 
-f = pyedflib.EdfReader(before_arith_task[1])
-print(f.signals_in_file) 
-eeg_signal = f.readSignal(0)
+def eeg_data_matrix(subject_idx, subject_edfsignal_dict):
+    #check if there's a edf file for a given subject index
+    if subject_edfsignal_dict.get(subject_idx,-1) == -1:
+        raise KeyError
+    #build data matrix
+    with pyedflib.EdfReader(subject_edfsignal_dict[subject_idx]) as f:
+        eeg_signals_arr = []
+        for idx in range(f.signals_in_file):
+            eeg_signal = f.readSignal(idx)
+            eeg_signals_arr.append(eeg_signal)
+        f.close()
+        return np.matrix(eeg_signals_arr)
+        
+#raw eeg-signals transformed into functional data using 
+#B-splines basis functions
+def data_2_funcdata(data_matrix):
+    freqs = []
+    fd = skfda.FDataGrid(
+            data_matrix=data_matrix,
+            grid_points=freqs
+        )
+    
+    return None
 
-print(eeg_signal)
