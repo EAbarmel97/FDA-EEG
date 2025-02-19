135b3cba3b9d579b4f011ce601fd2b62bfe6a69c
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2025-02-19 12:46:28 -0600

    fix: fixed bug in fetch_and_preprocess.sh

--- fetch_and_preprocess.sh
+++ fetch_and_preprocess.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-data_dir=$(pwd)"/data"
+data_dir="FDA-EEG/data"
 virtual_env=".venv/bin/activate"
 
 
 #if data dir doesnt exit
-if [ ! -d $(pwd)/"data" ]; then
+if [ ! -d "FDA-EEG/data" ]; then
   mkdir -p $data_dir/csv_data/{before_arith,after_arith}
 fi
 
@@ -12,7 +12,7 @@ mkdir -p $data_dir/csv_data/{before_arith,after_arith}
 
 #download EEG-data
 if [ ! -f "$data_dir/eeg-during-mental-arithmetic-tasks-1.0.0.zip" ]; then
-  curl -o "$data_dir/eeg-during-mental-arithmetic-tasks-1.0.0.zip" "https://physionet.org/static/published-projects/eegmat/eeg-during-mental-arithmetic-tasks-1.0.0.zip" 2> download.log
+  curl -o "$data_dir/eeg-during-mental-arithmetic-tasks-1.0.0.zip" "https://physionet.org/static/published-projects/eegmat/eeg-during-mental-arithmetic-tasks-1.0.0.zip" 2> ./download.log
 fi
 
 # searach .zip file and unzip it 
@@ -29,3 +29,4 @@ echo "procesing EDF files ..."
 
 #activate env and process EDF files to create CSV files
 source "$virtual_env" && python3 scripts/edf_preprocess.py
+
